f9e3168c5645e67aea74a071641426aabe7bcdcf
seb-faull <seb_faull@hotmail.co.uk>
2017-09-13 09:22:08 +0100

    Displaying numbers to the calculator implemented

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', function (event) {
 //All Global variables, loops etc gets put in here ---- for today...
 
-var num1 = [];
-var operator = false;
-var num2 = [];
+var num1 = NaN;
+var operator = NaN;
+var num2 = NaN;
+var ans = NaN;
+
 var numbers = document.getElementsByClassName('number');
 var operators = document.getElementsByClassName('operator');
 var equals = document.getElementsByClassName('eq');
 var cancel = document.getElementsByClassName('clear');
 
+var display = document.getElementsByClassName('display');
+
 function operators () {
 	for (i = 0; i < operators.length; i++) {
-		operators[i].addEventListener('click', function (event){
+		operators[i].addEventListener('click', function (event) {
 
 			var operatorInput = event.target.value
+
 			console.log(event.target.value);
 		})
 	}
@@ -29,22 +34,22 @@ function cancel () {
 
 function activateNumbers () {
 	for (i = 0; i < numbers.length; i++) {
-		numbers[i].addEventListener('click', function (event){
+		numbers[i].addEventListener('click', function (event) {
 
-			var numInput = event.target.value
+			// var numInput = event.target.value
 			// if (!operator) {
 			// 	num1Button = event.target.value;
-			// 	display[0].value = num1Button;
+			// 	display[i].value = num1Button;
 			// } else {
 			// 	num2Button = event.target.value;
-			// 	display[0].value = num2Button;
+			// 	display[i].value = num2Button;
 			// }
+			display[0].value = event.target.value;
 			console.log(event.target.value);
 		})
 	}
 }
 
 activateNumbers();
-	
 
 });
